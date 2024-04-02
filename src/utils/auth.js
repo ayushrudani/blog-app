c0d684ecb4ea115c4dfe6033836e87ec4085ec06
Ayush Rudani <ayushrudani9499@gmail.com>
2024-04-02 21:55:49 +0530

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -6,8 +6,8 @@ export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: "434620349749-42t9jiqr2kh4idh3g8taod87udumklmn.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: "",
+      clientSecret: "",
     }),
   ],
 };
